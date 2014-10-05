34f5e696f6a41225f5ee5a5348bcf906f0702f54
henrymound <henrymound@gmail.com>
2014-10-05 13:53:47 -0400

    Added the first 2 labs from class

---
+++ Lab1/Lab1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("My name is Henry Mound");
+            Console.WriteLine("My best friend's name is James");
+            Console.WriteLine("My favorite games are...");
+            Console.WriteLine("\t1. Chess");
+            Console.WriteLine("\t2. Monopoly");
+            Console.WriteLine("\t2. Statego");
+        }
+    }
+}
---
+++ Lab2/Lab2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+            age = 16;
+            Console.WriteLine("I am "+age+" years old.");
+
+            const int MAX_SCORE = 100;
+            int score = 35;
+            float percent = (float)score / MAX_SCORE;
+            Console.WriteLine("The percentage is: " + percent);
+        }
+    }
+}
