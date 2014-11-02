ace37b10079e190538af411b96b04b295d558382
henrymound <henrymound@gmail.com>
2014-11-01 22:15:41 -0400

    Added Lab8 and Lab9

---
+++ Lab8/Lab8/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String enteredData;
+            int pyramidSlotNumber;
+            char blockLetter;
+            Boolean litOrNot;
+
+            Console.WriteLine("Enter string in the format <pyramid slot number>,<block letter>,<whether or not the block should be lit> (x,y,z):");
+            enteredData = Console.ReadLine();
+            //Get variables from entered line
+            pyramidSlotNumber = int.Parse(enteredData.Substring(0, enteredData.IndexOf(",")));
+            blockLetter = enteredData[enteredData.IndexOf(",")+1];
+            litOrNot = Boolean.Parse(enteredData.Substring(5));
+            //Prints variables
+            Console.WriteLine("\nPyramid Slot Number: "+pyramidSlotNumber);
+            Console.WriteLine("Block Letter: "+blockLetter);
+            if (litOrNot)
+                Console.WriteLine("The block should be lit\n");
+            else
+                Console.WriteLine("The block should not be lit\n");
+        }
+    }
+}
---
+++ Lab9/Lab9/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputChoice = 0;
+
+            //Keeps the program running until the user chooses to quit
+            while (inputChoice != 4)
+            {
+
+                Console.WriteLine(
+                    "\n\n1 – New Game" +
+                    "\n2 – Load Game" +
+                    "\n3 – Options" +
+                    "\n4 – Quit" +
+                    "\n**************\n"
+                    );
+                Console.WriteLine("What would you like to do? (Enter corresponding menus number)");
+                inputChoice = int.Parse(Console.ReadLine());
+
+                //Responses based on conditionals
+                if (inputChoice == 1)
+                    Console.WriteLine("Starting a new game...");
+                else if (inputChoice == 2)
+                    Console.WriteLine("Loading game...");
+                else if (inputChoice == 3)
+                    Console.WriteLine("Displaying option menu...");
+                else if (inputChoice == 4)
+                    Console.WriteLine("Quitting...");
+                else if (inputChoice > 4 || inputChoice < 1)
+                    Console.WriteLine("Your response must be: 1, 2, 3, or 4. See menu.");
+
+                //Responses based on switch statement
+                switch (inputChoice)
+                {
+                    case 1:
+                        Console.WriteLine("Starting a new game...");
+                        break;
+                    case 2:
+                        Console.WriteLine("Loading game...");
+                        break;
+                    case 3:
+                        Console.WriteLine("Displaying option menu...");
+                        break;
+                    case 4:
+                        Console.WriteLine("Quitting...");
+                        break;
+                    default:
+                        Console.WriteLine("Your response must be: 1, 2, 3, or 4. See menu.");
+                        break;
+                }
+
+            }
+
+        }
+    }
+}
